7ee110ae86b26bd15f9107d1c06554ed723260d0
Farheen <farheen.ahmed2004@gmail.com>
2026-04-05 13:50:24 +0530

    update API endpoints to use production URL

--- Backend/controllers/company.controller.js
+++ Backend/controllers/company.controller.js
@@ -67,7 +67,11 @@ export const getCompanyById = async (req, res) => {
 export const updateCompany = async (req, res) => {
   try {
     const { name, description, website, location } = req.body;
+    
     const file = req.file;
+    if(!file){
+      return res.status(400).json({ message: "Logo is required" });
+    }
     //cloudinary
     const fileUri = getDataUri(file);
     const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,6 @@
-export const USER_API_ENDPOINT = "http://localhost:5011/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:5011/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:5011/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:5011/api/company";
+const BASE_URL = "https://job-portal-backend-qle9.onrender.com";
+
+export const USER_API_ENDPOINT = `${BASE_URL}/api/user`;
+export const JOB_API_ENDPOINT = `${BASE_URL}/api/job`;
+export const APPLICATION_API_ENDPOINT = `${BASE_URL}/api/application`;
+export const COMPANY_API_ENDPOINT = `${BASE_URL}/api/company`;
